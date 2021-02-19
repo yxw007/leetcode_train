d5dd29b720d2f6a84e5545347d67b0bff67e9697
Potter <75838259@qq.com>
2021-02-20 00:09:06 +0800

    @Potter : 第二天学习

---
+++ train_003.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    if (!s) {
+        return 0;
+    }
+    if (s.length <= 1) {
+        return s.length;
+    }
+
+    let arr = [...s];
+    let start = 0;
+    let end = 1;
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let str = arr.slice(start, end);
+        if (max < str.length) max = str.length;
+        let item = arr[i + 1];
+        const index = str.indexOf(item);
+        if (index !== -1) {
+            if (index === 0) {
+                start += 1;
+            } else {
+                start += index + 1;
+            }
+        }
+        end += 1;
+    }
+
+    return max;
+};
+
+let str = "pwwkew";
+let len = lengthOfLongestSubstring(str);
+console.log(len);
